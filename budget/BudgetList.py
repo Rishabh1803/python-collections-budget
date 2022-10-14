e4771d7efd44a2919d6ac9ccb6360399fb158eb3
Rishabh1803 <rishabhkhare18@gmail.com>
2022-10-15 00:21:29 +0530

    Update BudgetList.py
    Finish Making BudgetList an Iterable

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -4,6 +4,9 @@
 # Import the Expense module
 from . import Expense
 
+# Import Matplotlib
+import matplotlib.pyplot as plt
+
 # Create the BudgetList class
 class BudgetList():
 
@@ -33,6 +36,21 @@ class BudgetList():
     def __len__(self):
         return len(self.expenses) + len(self.overages)
 
+    # Create the __iter__() method
+    def __iter__(self):
+        # Finish __iter__()
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.overages)
+        return self
+
+    # Create next()
+    def __next__(self):
+        # Finsh __next__()
+        try:
+            return self.iter_e.__next__()
+        except StopIteration as stop:
+            return self.iter_o.__next__()
+
 
 # Define the main function
 def main():
@@ -47,6 +65,29 @@ def main():
         myBudgetList.append(expense.amount)
 
     print('The count of all expenses: ', str(len(myBudgetList)))
+    
+    # Test the iterable
+    for entry in myBudgetList:
+        print(entry)
+    
+    # Create the figures and axes
+    fig, ax = plt.subplots()
+
+    # Create the list of labels
+    labels = ['Expenses', 'Overages', 'Budget']
+
+    # Create the list of values
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+
+    # Create the bar graph
+    ax.bar(labels, values, color = ['green', 'red', 'blue'])
+
+    # Set the title
+    ax.set_title('Your total expenses vs. total budget')
+
+    # Show the figure
+    plt.show()
+
 
 # Tell Python to run the main function
 if __name__ == "__main__":
